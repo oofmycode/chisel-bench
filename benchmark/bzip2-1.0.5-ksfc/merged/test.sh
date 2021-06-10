0f50568b705bb5381c6754ce7016c4a18e8aa699
bruh <muazalijutt@gmail.com>
2021-06-11 03:49:59 +0500

    bzip2 test scripts added

--- benchmark/bzip2-1.0.5-ksfc/merged/test.sh
+++ benchmark/bzip2-1.0.5-ksfc/merged/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export BENCHMARK_NAME=bzip2-1.0.5
-export BENCHMARK_DIR=$CHISEL_BENCHMARK_HOME/benchmark/$BENCHMARK_NAME/merged
+export BIN_NAME=bzip2-1.0.5-ksfc
+export BENCHMARK_DIR=$CHISEL_BENCHMARK_HOME/benchmark/$BIN_NAME/merged
 export SRC=$BENCHMARK_DIR/$BENCHMARK_NAME.c
 export ORIGIN_BIN=$BENCHMARK_DIR/$BENCHMARK_NAME.origin
 export REDUCED_BIN=$BENCHMARK_DIR/$BENCHMARK_NAME.reduced
@@ -12,71 +13,41 @@ source $CHISEL_BENCHMARK_HOME/benchmark/test-base.sh
 
 function clean() {
   rm -rf -- *.bz2
-  rm -rf $LOG $REDUCED_BIN *.bz2 *.rb2 *.tst log foo* bar*
+  rm -rf $LOG $REDUCED_BIN *.bz2 *.rb2 *.tst log foo* bar* *.tmp testout.rb2
   rm -f \(*\)
   return 0
 }
-
+function run() {
+  { timeout $TIMEOUT $REDUCED_BIN $1 <$2 >testout.tmp; } &>$LOG || exit 1
+   bzip2 $1 <$2 > testout1.tmp  
+   cmp testout1.tmp testout.tmp >&/dev/null || exit 1
+   rm -rf *.tmp
+    return 0
+}
 function desired() {
-  # -c
-  { timeout $TIMEOUT $REDUCED_BIN -c <references/sample1.ref >sample1.rb2; } &>$LOG || exit 1
-  cmp references/sample1.bz2.ref sample1.rb2 >&/dev/null || exit 1
-  # -d
-  { timeout $TIMEOUT $REDUCED_BIN -d <references/sample1.bz2.ref >sample1.tst; } &>$LOG || exit 1
-  cmp references/sample1.ref sample1.tst >&/dev/null || exit 1
-  # -f
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -f foo; } >&$LOG || exit 1
-  # -t
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -t foo; } >&$LOG && exit 1
-  { timeout $TIMEOUT $REDUCED_BIN -t references/sample1.bz2.ref; } >&$LOG || exit 1
-  # -k
-  rm foo*
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -k foo; } >&$LOG || exit 1
-  test -f foo -a -f foo.bz2 || exit 1
-  return 0
+  # -ksfc
+  for file in $(ls test/*) ; do	  
+    run "-ksfc" $file || exit 1
+done
+return 0
 }
 
 function default_disaster_mem() {
-  # -c
-  { timeout $TIMEOUT $REDUCED_BIN -c <references/sample1.ref >sample1.rb2; } &>$LOG
+  # -ksfc
+  { timeout $TIMEOUT $REDUCED_BIN -ksfc <references/sample1.ref >testout.rb2; } &>$LOG
   grep -q -E "$1" $LOG || exit 1
-  # -d
-  { timeout $TIMEOUT $REDUCED_BIN -d <references/sample1.bz2.ref >sample1.tst; } &>$LOG
-  grep -q -E "$1" $LOG || exit 1
-  # -f
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -f foo; } >&$LOG
-  grep -q -E "$1" $LOG || exit 1
-  # -t
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -t foo; } >&$LOG
-  grep -q -E "$1" $LOG || exit 1
-
-  { timeout $TIMEOUT $REDUCED_BIN -t references/sample1.bz2.ref; } >&$LOG
-  grep -q -E "$1" $LOG || exit 1
-  # -k
-  rm foo*
-  echo "1234" >foo
-  { timeout $TIMEOUT $REDUCED_BIN -k foo; } >&$LOG
-  grep -q -E "$1" $LOG || exit 1
-  return 0
+return 0
 }
 
 function default_disaster_file() {
-  # -c
-  { timeout $TIMEOUT $REDUCED_BIN -c <references/sample1.ref >sample1.rb2; } &>$LOG
-  grep -q -E "$1" $LOG || exit 1
-  # -d
-  { timeout $TIMEOUT $REDUCED_BIN -d <references/sample1.bz2.ref >sample1.tst; } &>$LOG
+  # -ksfc
+  { timeout $TIMEOUT $REDUCED_BIN -ksfc <references/sample1.ref >testout.rb2; } &>$LOG
   grep -q -E "$1" $LOG || exit 1
   return 0
 }
 
 OPT1=("-h")
-OPT2=("-z" "-q" "-v" "-s" "-1" "-2" "-3" "-4" "-5" "-6" "-7" "-8" "-9")
+OPT2=("-z" "-q" "-v" "-1" "-2" "-3" "-4" "-5" "-6" "-7" "-8" "-9")
 OPT3=("-L" "-V")
 function undesired() {
   { timeout $TIMEOUT $REDUCED_BIN $(cat crash_input); } &>$LOG
@@ -113,11 +84,9 @@ function undesired() {
   done
 
   echo "1234" >foo
-  $REDUCED_BIN -c <foo >foo.bz2 || exit 1
+  $REDUCED_BIN -ksfc <foo >foo.bz2 || exit 1
   for opt in ${OPT2[@]}; do
-    { timeout $TIMEOUT $REDUCED_BIN -d $opt <foo.bz2 >sample1.tst; } 2>$LOG
-    crash $? && exit 1
-    { timeout $TIMEOUT $REDUCED_BIN -c $opt <references/sample1.ref >sample1.rb2; } 2>$LOG
+        { timeout $TIMEOUT $REDUCED_BIN -ksfc $opt <references/sample1.ref >testout.rb2; } 2>$LOG
     crash $? && exit 1
   done
 
